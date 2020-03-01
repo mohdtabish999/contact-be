b7e9066d814cdf63f81ffa77a521066de947b5ec
tabish99 <mohdtabish99@gmail.com>
2020-03-01 14:35:56 +0530

    committed project

---
+++ src/main/java/com/AddressRepository.java
@@ -0,0 +1,10 @@
+/*package com;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entity.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+}
+*/
